3aec885bbf5abbc18c0434e2cc137a2b2defa7f8
dongbin <dong.firstname@qq.com>
2021-02-26 10:15:46 +0800

    test

---
+++ common/common_utils/src/main/java/com/atguigu/commonutils/ResultCode.java
@@ -0,0 +1,17 @@
+package com.atguigu.commonutils;
+
+/**
+ * @author db
+ * @date 2021/1/15 - 16:14
+ */
+public interface ResultCode {
+    /**
+     * 成功
+     */
+    public static Integer SUCCESS = 20000;
+    /**
+     * 失败
+     */
+    public static Integer ERROR   = 20001;
+
+}
